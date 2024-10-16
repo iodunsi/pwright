974856d3b5ebb7c050896e18a5ef9b76423d5d86
Jay <jhr4@njit.edu>
2024-10-16 16:37:19 -0400

    added tests in accordance to Acceptance Criteria

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,9 +1,49 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-test('has title', async ({ page }) => {
-  await page.goto('http://localhost:4000/');
+test('There should be a title stating “Student Guide to Docker”', async ({ page }) => {
+  await page.goto('localhost:4000');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Hexo/);
+  // Checking title of landing page
+  await expect(page).toHaveTitle(/Student Guide to Docker/);
+  
+});
+
+
+test('Check for mention of docker using key terms: check for words “containerization,” “docker image,” and “kernel”', async ({ page }) => {
+  await page.goto('https://jhr-4.github.io/IS373_Hexo/README/');
+
+  //Look in the class of the article for key terms
+  await expect(page.locator('.article-inner')).toHaveText(/Container/);
+  await expect(page.locator('.article-inner')).toHaveText(/Docker Image/);
+  await expect(page.locator('.article-inner')).toHaveText(/kernel/);
+  
+});
+
+
+test('There should be clickable hyperlinks to all sub pages/articles', async ({ page }) => {
+  await page.goto('https://jhr-4.github.io/IS373_Hexo/README/');
+
+  //Find page link, click it, check title to see if page works, and then go back
+
+  //Virtualization VS Containerization 
+  await page.locator('.article-inner').locator('#Virtualization-vs-Containerization').getByText('Virtualization vs. Containerization').click();
+  await expect(page.locator('.article-header')).toHaveText(/Virtualization vs. Containerization/);
+  await page.locator('#sidebar').getByText(/Home/).click();
+
+  //Docker vs Kubernetes
+  await page.locator('.article-inner').locator('#Docker-vs-Kubernetes').getByText('Docker vs. Kubernetes').click();
+  await expect(page.locator('.article-header')).toHaveText(/Docker vs Kubernetes/);
+  await page.locator('#sidebar').getByText(/Home/).click();
+
+  //Installation
+  await page.locator('.article-inner').locator('#Installation-Process').getByText('Installation Process').click();
+  await expect(page.locator('.article-header')).toHaveText(/Installation & Setup/);
+  await page.locator('#sidebar').getByText(/Home/).click();
+
+  //CI-CD
+  await page.locator('.article-inner').locator('#CI-CD-Explained-Setup').getByText('CI/CD Explained & Setup').click();
+  await expect(page.locator('.article-header')).toHaveText(/CI\/CD Explained & Setup/);
+  await page.locator('#sidebar').getByText(/Home/).click();
+  
 });
